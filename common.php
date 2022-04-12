4d791670e2641590f7d8c7fa7edf6060348f6fe6
theplenoh <tylrd529@gmail.com>
2022-04-13 00:21:48 +0900

    Add function library

--- common.php
+++ common.php
@@ -10,4 +10,22 @@ header("Content-Type: text/html; charset={$charset}");
 // Display all errors
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
+
+// Function library
+function sanitize($text)
+{
+    return htmlentities(addslashes($text));
+}
+
+function filter($text)
+{
+    // <https://stackoverflow.com/questions/4144837/auto-link-urls-in-a-string>
+    $text = preg_replace('/((http|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?)/', '<a href="\1">\1</a>', $text);
+
+    $text = str_replace("--", "&mdash;", $text);
+
+    $text = nl2br($text);
+
+    return $text;
+}
 ?>
