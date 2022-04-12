806c53d47b19809bef80185d48600070737ea38c
theplenoh <tylrd529@gmail.com>
2022-04-12 16:53:27 +0900

    Add a basic common.php file

---
+++ common.php
@@ -0,0 +1,13 @@
+<?php
+// User variable configuration
+$path = "";
+$charset = "utf-8";
+$site_title = "";
+
+// Encoding
+header("Content-Type: text/html; charset={$charset}");
+
+// Display all errors
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+?>
