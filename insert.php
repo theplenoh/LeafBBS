af95460ebcee6f7687b1165b1dc285661279062a
theplenoh <tylrd529@gmail.com>
2022-04-19 15:39:41 +0900

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,15 +1,15 @@
 <?php
 require_once "common.php";
 
-require_once $path."db_info.php";
+require_once $path."db_connect.php";
 
 // Get the value of max_thread
-$query = "SELECT max(thread) FROM {$board}";
+$query = "SELECT max(thread) FROM {$board_name}";
 $max_thread_result = mysqli_query($conn, $query);
 $max_thread_fetch = mysqli_fetch_row($max_thread_result);
 $max_thread = ceil($max_thread_fetch[0]/1000)*1000 + 1000;
 
-$query = "INSERT INTO {$board} (thread, depth, name, password, email, title, views, datetime, ipaddr, content) ";
+$query = "INSERT INTO {$board_name} (thread, depth, name, password, email, title, views, datetime, ipaddr, content) ";
 $query.= "VALUES ({$max_thread}, 0, '{$_POST[name]}', '{$_POST[password]}', '{$_POST[email]}', '{$_POST[title]}', 0, UNIX_TIMESTAMP(), '{$_SERVER[REMOTE_ADDR]}', '{$_POST[content]}')";
 $result = mysqli_query($conn, $query);
 
