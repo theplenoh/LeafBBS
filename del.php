690f63ae1e06ef00a21ddc7b7a604ae455cf5a5a
theplenoh <tylrd529@gmail.com>
2022-05-02 21:59:27 +0900

    Add delete post feature

---
+++ del.php
@@ -0,0 +1,36 @@
+<?php
+require_once "common.php";
+
+require_once $path."db_connect.php";
+
+$postID = $_GET['id'];
+$password = $_POST['password'];
+
+$result = mysqli_query($conn, "SELECT password FROM {$board_name} WHERE postID={$postID}");
+$row = mysqli_fetch_array($result);
+
+if ($password == $row['password'])
+{
+    $query = "DELETE FROM {$board_name} WHERE postID={$postID}";
+    $result = mysqli_query($conn, $query);
+
+    $success = true;
+}
+else
+{
+?>
+<script>
+alert("Wrong password!");
+history.go(-1);
+</script>
+<?php
+exit;
+}
+?>
+<script>
+if (<?=$success?>)
+{
+alert("Successfully deleted!");
+location.href="<?=$path?>list.php";
+}
+</script>
---
+++ pre_del.php
@@ -0,0 +1,35 @@
+<?php
+require_once "common.php";
+$page_title = "Delete Post";
+
+require_once $path."db_connect.php";
+
+$postID = $_GET['id'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="<?=$charset?>">
+    <title><?=$page_title?> :: <?=$site_title?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="<?=$path?>style.css">
+</head>
+<body>
+<div id="container">
+<h1>Password Confirm before Deleting</h1>
+<form action="del.php?id=<?=$postID?>" method="post">
+<dl>
+    <dt>Password</dt>
+    <dd>
+        <input type="password" name="password" size="16">
+    </dd>
+</dl>
+<p>
+    <input type="submit" value="Confirm">
+    <a href="<?=$path?>list.php">Cancel</a>
+</p>
+</form>
+</div>
+</body>
+</html>
