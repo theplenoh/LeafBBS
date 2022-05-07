e821874c7066ecea56d639bfc105b5cd0efbf05f
theplenoh <tylrd529@gmail.com>
2022-05-07 13:32:33 +0900

    Improve insert.php

--- insert.php
+++ insert.php
@@ -3,6 +3,13 @@ require_once "common.php";
 
 require_once $path."db_connect.php";
 
+$posted_by = sanitize($_POST['name']);
+$password = sanitize($_POST['password']);
+$email = sanitize($_POST['email']);
+$title = sanitize($_POST['title']);
+$content = filter(sanitize($_POST['content']));
+$ip_addr = $_SERVER[REMOTE_ADDR];
+
 // Get the value of max_thread
 $query = "SELECT max(thread) FROM {$board_name}";
 $max_thread_result = mysqli_query($conn, $query);
@@ -10,8 +17,14 @@ $max_thread_fetch = mysqli_fetch_row($max_thread_result);
 $max_thread = ceil($max_thread_fetch[0]/1000)*1000 + 1000;
 
 $query = "INSERT INTO {$board_name} (thread, depth, name, password, email, title, views, datetime, ipaddr, content) ";
-$query.= "VALUES ({$max_thread}, 0, '{$_POST[name]}', '{$_POST[password]}', '{$_POST[email]}', '{$_POST[title]}', 0, UNIX_TIMESTAMP(), '{$_SERVER[REMOTE_ADDR]}', '{$_POST[content]}')";
-$result = mysqli_query($conn, $query);
+$query.= "VALUES ({$max_thread}, 0, '{$posted_by}', '{$password}', '{$email}', '{$title}', 0, UNIX_TIMESTAMP(), '{$ip_addr}', '{$content}')";
+//$result = mysqli_query($conn, $query);
+if (mysqli_query($conn, $query)) {
+    echo "Successfully submitted!";
+}
+else {
+    echo mysqli_error($conn);
+}
 
 // MySQL connection close
 mysqli_close($conn);
@@ -21,6 +34,6 @@ $success = true;
 <script>
 if (<?=$success?>) {
     alert("Successfully submitted!");
-    window.location.href="<?=$path?>list.php";
+    location.href="<?=$path?>list.php";
 }
 </script>
