314f732a35865971dc407fd08d9b9ee652a03b8c
theplenoh <tylrd529@gmail.com>
2022-05-09 18:23:23 +0900

    Add reply.php

---
+++ reply.php
@@ -0,0 +1,63 @@
+<?php
+require_once "common.php";
+$page_title = "Reply to";
+
+require_once $path."db_connect.php";
+
+$postID = $_GET['id'];
+
+$query = "SELECT * FROM {$board_name} WHERE postID='{$postID}'";
+$parent_result = mysqli_query($conn, $query);
+$parent_row = mysqli_fetch_array($parent_result);
+$parent_title = "RE: ".$parent_row['title'];
+$parent_content = "\n&gt;".str_replace("\n", "\n&gt;", $parent_row['content']);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="<?=$charset?>">
+    <title><?=$page_title?> :: <?=$site_title?></title>
+
+    <link rel="stylesheet" type="text/css" href="<?=$path?>style.css">
+</head>
+
+<body>
+<div id="container">
+<h1><?=$page_title?> :: <?=$site_title?></h1>
+<form action="<?=$path?>insert_reply.php" method="post">
+<input type="hidden" name="parent_depth" value="<?=$parent_row['depth']?>">
+<input type="hidden" name="parent_thread" value="<?=$parent_row['thread']?>">
+<dl>
+    <dt><label for="name">Name</label></dt>
+    <dd>
+        <input type="text" name="name" id="name" maxlength="6">
+    </dd>
+    <dt><label for="password">Password</label></dt>
+    <dd>
+        <span class="warning">
+            Enter a temporary password.<br>
+            (Needed later for editiing/deleting this post)
+        </span><br>
+        <input type="password" name="password" id="password" maxlength="16">
+    </dd>
+    <dt><label for="email">E-mail</label></dt>
+    <dd>
+        <input type="text" name="email" id="email" maxlength="30">
+    </dd>
+    <dt><label for="title">Title</label></dt>
+    <dd>
+        <input type="text" name="title" id="title" maxlength="23" value="<?=$parent_title?>">
+    </dd>
+    <dt><label for="content">Content</label></dt>
+    <dd>
+        <textarea name="content" id="content"><?=$parent_content?></textarea>
+    </dd>
+</dl>
+<p>
+    <input type="submit" value="Submit">
+    <a href="<?=$path?>list.php">List</a>
+</p>
+</form>
+</div>
+</body>
+</html>
