282d10f5e231c9b5d2d4a7cc66ee0659bd026a24
theplenoh <tylrd529@gmail.com>
2022-05-10 20:42:55 +0900

    Improve edit.php

--- edit.php
+++ edit.php
@@ -5,13 +5,17 @@ require_once $path."db_connect.php";
 
 $postID = $_GET['post_id'];
 $password = $_POST['password'];
+$posted_by = $_POST['name'];
+$email = $_POST['email'];
+$title = $_POST['title'];
+$content = $_POST['content'];
 
 $result = mysqli_query($conn, "SELECT password FROM {$board_name} WHERE postID={$postID}");
 $row = mysqli_fetch_array($result);
 
 if ($password == $row['password'])
 {
-    $query = "UPDATE {$board_name} SET name='{$_POST[name]}', email='$_POST[email]', title='$_POST[title]', content='$_POST[content]' WHERE postID={$postID}";
+    $query = "UPDATE {$board_name} SET name='{$posted_by}', email='{$email}', title='{$title}', content='{$content}' WHERE postID={$postID}";
     $result = mysqli_query($conn, $query);
 
     $success = true;
