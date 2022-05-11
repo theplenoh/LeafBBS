b2f8884f1bccca2c6c899777e49d3a8d842b9437
theplenoh <tylrd529@gmail.com>
2022-05-11 15:07:20 +0900

    Sanitize info accepted from user

--- edit.php
+++ edit.php
@@ -4,11 +4,11 @@ require_once "common.php";
 require_once $path."db_connect.php";
 
 $postID = $_GET['post_id'];
-$password = $_POST['password'];
-$posted_by = $_POST['name'];
-$email = $_POST['email'];
-$title = $_POST['title'];
-$content = $_POST['content'];
+$password = sanitize($_POST['password']);
+$posted_by = sanitize($_POST['name']);
+$email = sanitize($_POST['email']);
+$title = sanitize($_POST['title']);
+$content = sanitize($_POST['content']);
 
 $result = mysqli_query($conn, "SELECT password FROM {$board_name} WHERE postID={$postID}");
 $row = mysqli_fetch_array($result);
