7b5d4c18534bfea1ec2885c34189d5e96f0022da
theplenoh <tylrd529@gmail.com>
2022-11-11 11:21:28 +0900

    Change the format of page title(list.php)

--- list.php
+++ list.php
@@ -21,7 +21,7 @@ $page_max = ceil($total / $page_size);
 <html>
 <head>
     <meta charset="<?=$charset?>">
-    <title><?=$page_title?> :: <?=$site_title?></title>
+    <title><?=$board_title?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" type="text/css" href="<?=$path?>styles/<?=$skin_name?>/style.css">
