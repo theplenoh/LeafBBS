aab1d5dab063fcb997ad0855b177c4a6113841b5
theplenoh <tylrd529@gmail.com>
2022-11-25 15:55:29 +0900

    Renew reply.php page

--- reply.php
+++ reply.php
@@ -16,7 +16,7 @@ $parent_content = "\n&gt;".str_replace("\n", "\n&gt;", $parent_row['content']);
 <html>
 <head>
     <meta charset="<?=$charset?>">
-    <title><?=$page_title?> :: <?=$site_title?></title>
+    <title><?=$board_title?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" type="text/css" href="<?=$path?>styles/<?=$skin_name?>/style.css">
@@ -24,41 +24,46 @@ $parent_content = "\n&gt;".str_replace("\n", "\n&gt;", $parent_row['content']);
 
 <body>
 <div id="leaf">
-<h1><?=$page_title?> :: <?=$site_title?></h1>
-<form action="<?=$path?>insert_reply.php" method="post">
-<input type="hidden" name="parent_depth" value="<?=$parent_row['depth']?>">
-<input type="hidden" name="parent_thread" value="<?=$parent_row['thread']?>">
-<dl>
-    <dt><label for="name">Name</label></dt>
-    <dd>
-        <input type="text" name="name" id="name" maxlength="6">
-    </dd>
-    <dt><label for="password">Password</label></dt>
-    <dd>
-        <span class="warning">
-            Enter a temporary password.<br>
-            (Needed later for editiing/deleting this post)
-        </span><br>
-        <input type="password" name="password" id="password" maxlength="16">
-    </dd>
-    <dt><label for="email">E-mail</label></dt>
-    <dd>
-        <input type="text" name="email" id="email" maxlength="30">
-    </dd>
-    <dt><label for="title">Title</label></dt>
-    <dd>
-        <input type="text" name="title" id="title" maxlength="23" value="<?=$parent_title?>">
-    </dd>
-    <dt><label for="content">Content</label></dt>
-    <dd>
-        <textarea name="content" id="content"><?=$parent_content?></textarea>
-    </dd>
-</dl>
-<p>
-    <input type="submit" value="Submit">
-    <a href="<?=$path?>view.php?post_id=<?=$postID?>">Cancel</a>
-</p>
-</form>
+<h1><?=$board_title?></h1>
+<main>
+    <h2>Reply to a Post</h2>
+    <form action="<?=$path?>insert_reply.php" method="post">
+    <input type="hidden" name="parent_depth" value="<?=$parent_row['depth']?>">
+    <input type="hidden" name="parent_thread" value="<?=$parent_row['thread']?>">
+    <dl>
+        <dt><label for="name">Name</label></dt>
+        <dd>
+            <input type="text" name="name" id="name" maxlength="6">
+        </dd>
+        <dt><label for="password">Password</label></dt>
+        <dd>
+            <div class="warning">
+                <small>
+                Enter a temporary password.<br>
+                (Needed later for editiing/deleting this post)
+                </small>
+            </div>
+            <input type="password" name="password" id="password" maxlength="16">
+        </dd>
+        <dt><label for="email">E-mail</label></dt>
+        <dd>
+            <input type="text" name="email" id="email" maxlength="30">
+        </dd>
+        <dt><label for="title">Title</label></dt>
+        <dd>
+            <input type="text" name="title" id="title" maxlength="23" value="<?=$parent_title?>">
+        </dd>
+        <dt><label for="content">Content</label></dt>
+        <dd>
+            <textarea name="content" id="content"><?=$parent_content?></textarea>
+        </dd>
+    </dl>
+    <p class="buttons">
+        <input class="btn" type="submit" value="Reply">
+        <a class="btn" href="<?=$path?>view.php?post_id=<?=$postID?>">Cancel</a>
+    </p>
+    </form>
+</main>
 </div>
 </body>
 </html>
