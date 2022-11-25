22e2c8a2aa8d833348d75d3fc6df18984e0d93d1
theplenoh <tylrd529@gmail.com>
2022-11-25 15:55:53 +0900

    Renew pre_del.php page

--- pre_del.php
+++ pre_del.php
@@ -10,26 +10,29 @@ $postID = $_GET['post_id'];
 <html>
 <head>
     <meta charset="<?=$charset?>">
-    <title><?=$page_title?> :: <?=$site_title?></title>
+    <title><?=$board_title?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" type="text/css" href="<?=$path?>styles/<?=$skin_name?>/style.css">
 </head>
 <body>
 <div id="leaf">
-<h1>Password Confirm before Deleting</h1>
-<form action="<?=$path?>del.php?post_id=<?=$postID?>" method="post">
-<dl>
-    <dt>Password</dt>
-    <dd>
-        <input type="password" name="password" size="16">
-    </dd>
-</dl>
-<p>
-    <input type="submit" value="Confirm">
-    <a href="<?=$path?>view.php?post_id=<?=$postID?>">Cancel</a>
-</p>
-</form>
+<h1><?=$board_title?></h1>
+<main>
+    <h2>Password Confirm before Deleting</h2>
+    <form action="<?=$path?>del.php?post_id=<?=$postID?>" method="post">
+    <dl>
+        <dt>Password</dt>
+        <dd>
+            <input type="password" name="password" size="16">
+        </dd>
+    </dl>
+    <p class="buttons">
+        <input type="submit" class="btn" value="Delete">
+        <a href="<?=$path?>view.php?post_id=<?=$postID?>" class="btn">Cancel</a>
+    </p>
+    </form>
+</main>
 </div>
 </body>
 </html>
