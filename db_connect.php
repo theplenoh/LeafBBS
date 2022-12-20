69fe94c371de997f058f13504775248cfb596db9
theplenoh <tylrd529@gmail.com>
2022-12-20 17:52:31 +0900

    Error handling in case of DB connection failure

--- db_connect.php
+++ db_connect.php
@@ -2,6 +2,6 @@
 require_once "common.php";
 require_once "db_info.php";
 
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$conn = @mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die("Cannot connect to the database");
 mysqli_query($conn, "SET NAMES utf8");
 ?>
